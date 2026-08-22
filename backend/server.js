5e63b0408f33f935007a7b08b3509dd8aef6cf16
richard amofa <amofarichard101@gmail.com>
2026-08-22 10:13:18 +0000

    Prisma error handling | v.6

--- backend/server.js
+++ backend/server.js
@@ -148,7 +148,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
-    `🚀 Server running on port ${PORT}`,
+    `🕷️  Server running on port ${PORT}`,
   );
 });
 
